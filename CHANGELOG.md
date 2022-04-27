e24836bfd5adfc05e16bc591f82c7e4bd362f154
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-27 15:52:15 +0200

    0.1.0 auto generated release
    Feature
    
    * move code from groupme (d19e8cfa0e27)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,10 @@
 
 Every noteable change is logged here.
 
+## v0.1.0
+
+### Feature
+
+* move code from groupme (d19e8cfa0e27)
+
 ## v0.0.0 Initial release
--- reftable/__init__.py
+++ reftable/__init__.py
@@ -11,7 +11,7 @@ import os
 
 import reftable.path
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PROCESS = 'reftable'
