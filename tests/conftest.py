16ef9d41e65c6c0b190995f904b61e6026945a60
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-09-29 18:42:03 +0200

    chore(ci): increase worker count on CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@
 
 import genex
 import power
+import utilatest
 from utilatest import mp  # pylint:disable=W0611
 from utilatest import td  # pylint:disable=W0611
 
@@ -95,7 +96,7 @@ RESOURCES = [
     ),
 ]
 
-WORKER = 5
+WORKER = utilatest.worker_count(6, onci=len(RESOURCES))
 
 
 def pytest_sessionstart(session):  # pylint:disable=W0613
