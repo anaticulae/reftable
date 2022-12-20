2d22a25b8301313622bc1fafb9ff8f0e5559eca7
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-20 23:43:36 +0000

    chore(setup): use quick installer

--- setup.py
+++ setup.py
@@ -8,50 +8,20 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'reftable/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'reftable',
+    'reftable.abbrev',
+    'reftable.feature',
+    'reftable.figure',
+    'reftable.toc',
+    'reftable.toc.basic',
+    'reftable.toc.strategy',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'reftable = reftable.cli:main',
+])
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='nice to know',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='reftable',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/reftable',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'reftable',
-            'reftable.abbrev',
-            'reftable.feature',
-            'reftable.figure',
-            'reftable.toc',
-            'reftable.toc.basic',
-            'reftable.toc.strategy',
-        ],
-        entry_points={
-            'console_scripts': ['reftable = reftable.cli:main',],
-        },
-    )
+    utila.install(__file__)
